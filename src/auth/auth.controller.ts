c799671ba90ed1ad5adc8418c64879bfe2d4e7ad
Quang Khánh <khanhnguyenmcu@gmail.com>
2022-10-13 16:27:56 +0700

    Remove spec file, add ldap file

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -7,6 +7,7 @@ import { AuthDto } from './dto';
 export class AuthController {
     constructor(private readonly authService: AuthService) {};
 
+    // TO-DO: use ldap guard, keep username and password (not necessarily AuthDto) to pass to signin
     @Post('signin')
     async signin(@Body() authDto: AuthDto, @Res({passthrough: true}) res: Response) {
         return this.authService.signin(authDto, res);
