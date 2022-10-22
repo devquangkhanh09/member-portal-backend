a26cc998c8818e2773162b288eb0bccd75cc503f
Quang Khánh <khanhnguyenmcu@gmail.com>
2022-10-22 13:15:24 +0700

    Implement getUserProfile with user object

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,61 +1,44 @@
 import { HttpService } from '@nestjs/axios';
 import { HttpException, Injectable } from '@nestjs/common';
 import { Request } from 'express';
-import { catchError, map, throwError } from 'rxjs';
+import { catchError, map, tap, throwError } from 'rxjs';
 
 @Injectable()
 export class UserService {
-    private cookies;
-    private book;
     constructor(private readonly httpService: HttpService) {}
 
-    // TO-DO: implement getUserProfile to return an object of UserSchema
-    async getIDByName(name: string) {
-
-        return await this.httpService.get('https://jira.hpcc.vn/rest/insight/1.0/iql/objects',{
+    // TO-DO: combine with jwt strategy (replace username in req.query)
+    async getUserProfile(req: Request) {    
+        return this.httpService.get('https://jira.hpcc.vn/rest/insight/1.0/iql/objects', {
             headers: {
-                Cookie: `JSESSIONID=${this.cookies['JSESSIONID']}; atlassian.xsrf.token=${this.cookies['atlassian.xsrf.token']}`
+                Cookie: `JSESSIONID=${req.cookies['JSESSIONID']}; atlassian.xsrf.token=${req.cookies['atlassian.xsrf.token']}`
+            },
+            params: {
+                iql: `label=${req.query.username}`
             }
         })
             .pipe(
-                catchError(err => throwError(() => new HttpException('cannot retrieve object', 400))),
-                map(response => response.data['objectEntries']),
-                map(response => response.filter(res => res['objectType']['name'] === "HPC LIBRARY")),
-                map(response => response.filter(res => res['label'] === name)),
-                map(response => response[0]['id'])
-            );
-    }
-
-    async getInfoByID(ID) {
-        var tmp = await this.httpService.get(`https://jira.hpcc.vn/rest/insight/1.0/object/${ID}/attributes`, {
-            headers: {
-                Cookie: `JSESSIONID=${this.cookies['JSESSIONID']}; atlassian.xsrf.token=${this.cookies['atlassian.xsrf.token']}`
-            }
-        })
-            .pipe(
-                catchError(err => throwError(() => new HttpException('cannot retrieve object', 400))),
+                catchError(err => throwError(() => new HttpException('Jira: cannot retrieve object', 400))),
+                tap(response => {
+                    if (response.data.objectEntries.length === 0) {
+                        throw new HttpException('Jira: object not found', 404);
+                    }
+                }),
                 map(response => response.data),
-                map(response => {
-                    var result = {};
-                    response.forEach(element => {
-                    result[element['objectTypeAttribute']['name']] = element['objectAttributeValues'][0]['value'];
+                map(data => {
+                    const entry = data.objectEntries[0];
+                    const attributes = data.objectTypeAttributes;
+                    
+                    const res = {};
+                    attributes.forEach((attribute, idx) => {
+                        res[attribute['name']] = entry['attributes'][idx]['objectAttributeValues'][0]['value'];
+                    });
+                    
+                    return {
+                        avatar: entry['avatar'],
+                        ...res
+                    };
                 })
-                return result;
-            })
             )
-        
-        tmp.subscribe(val => {
-            this.book = val;
-            console.log(this.book);
-        })
-    }
-
-    async getUserProfile(req: Request) {
-        /*if (req.cookies == undefined){
-            return new HttpException('cannot retrieve object', 400);
-        }*/
-        this.cookies = req.cookies;
-        var ID = this.getIDByName(req.body.username);
-        (await ID).subscribe(val => this.getInfoByID(val));
     }
 }
