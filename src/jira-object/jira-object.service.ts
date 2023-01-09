f60df3d863b258326435bd9a32097029c1588a92
Quang Khánh <khanhnguyenmcu@gmail.com>
2023-01-09 19:34:57 +0700

    Remove unnecessary return statement

--- src/jira-object/jira-object.service.ts
+++ src/jira-object/jira-object.service.ts
@@ -38,7 +38,6 @@ export class JiraObjectService {
                         avatar: entry['avatar'],
                         ...res
                     };
-                    return data;
                 })
             )
     }
